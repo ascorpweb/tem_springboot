7390cd9c06495da28e3887e34994fa29f34752a9
ascorpweb <ascorpwebhosting@gmail.com>
2022-12-31 11:10:27 +0530

    added template

---
+++ src/main/java/com/airasia/searchxp/SearchxpApplication.java
@@ -0,0 +1,14 @@
+package com.airasia.searchxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchxpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchxpApplication.class, args);
+    }
+
+}
+//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
---
+++ src/test/java/com/airasia/searchxp/SearchxpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airasia.searchxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchxpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
